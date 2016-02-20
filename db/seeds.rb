142b94c3a25f25915545bf49b69373f1a10d6da5
Shohei Kameda <shoheik@cpan.org>
2016-02-20 15:42:13 +0900

    changed seed for default, which created_by is nil

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Blocker.create!([
-  {title: "Block all images", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"resource-type\":[\"image\"]}}]", count: 0, created_by: 1},
-  {title: "Block third-party", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"load-type\":[\"third-party\"]}}]", count: 0, created_by: 1},
-  {title: "Block all images and webfont", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"resource-type\":[\"image\",\"font\"]}}]", count: 0, created_by: 1},
+  {title: "Block all images", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"resource-type\":[\"image\"]}}]", count: 0, created_by: nil},
+  {title: "Block third-party", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"load-type\":[\"third-party\"]}}]", count: 0, created_by: nil},
+  {title: "Block all images and webfont", rule: "[{\"action\":{\"type\":\"block\"},\"trigger\":{\"url-filter\":\".*\",\"resource-type\":[\"image\",\"font\"]}}]", count: 0, created_by: nil},
 ])
